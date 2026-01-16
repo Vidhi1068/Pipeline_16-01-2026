7275da37a086c8b73e33df43a0a043ab2848bf8d
Vidhi <vidhinayak2@gmail.com>
2026-01-16 11:53:22 +0530

    trying to push automation code

---
+++ src/math_operation.py
@@ -0,0 +1,19 @@
+# Function for addition
+def add(a, b):
+    return a + b
+
+# Function for subtraction
+def subtract(a, b):
+    return a - b
+
+# Calling the functions
+x = 10
+y = 5
+
+sum_result = add(x, y)
+sub_result = subtract(x, y)
+
+# Printing the results
+print("Addition:", sum_result)
+print("Subtraction:", sub_result)
+
